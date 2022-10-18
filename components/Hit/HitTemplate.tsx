0dda2cbc07d0491d9e2ad248e4e9d2a7c5af81ee
Elías Heresi <elias.heresi@gmail.com>
2022-10-18 15:23:13 -0300

    Added information about the author

--- components/Hit/HitTemplate.tsx
+++ components/Hit/HitTemplate.tsx
@@ -3,11 +3,18 @@ import { FunctionComponent } from "react";
 import styles from "../../styles/HitTemplate.module.css";
 
 export const HitTemplate: FunctionComponent<any> = ({ hit }) => {
-  const { title, status, rating, image } = hit;
+  const {
+    title,
+    status,
+    rating,
+    image,
+    author: { name: authorName },
+  } = hit;
 
   return (
     <div className={styles.container}>
       <h1>{title}</h1>
+      <h2>{authorName}</h2>
       <p>Rating: {rating ? `${rating}/5` : "N/A"}</p>
       <Image
         src={`http:${image.file.url}`}
